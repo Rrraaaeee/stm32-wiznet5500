8301c0f6f51700f0035623342ed8ee13a943e91e
Rrraaaeee <qingxuannn0602@gmail.com>
2021-09-03 01:28:25 +0800

    First commit. Able to connect with telnet and ping host

---
+++ Debug/Core/Src/ping.d
@@ -0,0 +1,12 @@
+Core/Src/ping.o: ../Core/Src/ping.c ../Core/Inc/ping.h \
+ /Users/raeee/STM32CubeIDE/BBAUV/WiznetTest/Drivers/Wiznet_Driver/Ethernet/wizchip_conf.h \
+ /Users/raeee/STM32CubeIDE/BBAUV/WiznetTest/Drivers/Wiznet_Driver/Ethernet/../../Wiznet_Driver/Ethernet/W5500/w5500.h \
+ /Users/raeee/STM32CubeIDE/BBAUV/WiznetTest/Drivers/Wiznet_Driver/Ethernet/../../Wiznet_Driver/Ethernet/W5500/../wizchip_conf.h
+
+../Core/Inc/ping.h:
+
+/Users/raeee/STM32CubeIDE/BBAUV/WiznetTest/Drivers/Wiznet_Driver/Ethernet/wizchip_conf.h:
+
+/Users/raeee/STM32CubeIDE/BBAUV/WiznetTest/Drivers/Wiznet_Driver/Ethernet/../../Wiznet_Driver/Ethernet/W5500/w5500.h:
+
+/Users/raeee/STM32CubeIDE/BBAUV/WiznetTest/Drivers/Wiznet_Driver/Ethernet/../../Wiznet_Driver/Ethernet/W5500/../wizchip_conf.h:
---
+++ Debug/Drivers/Wiznet_Driver/Ethernet/W5500/w5500.d
@@ -0,0 +1,11 @@
+Drivers/Wiznet_Driver/Ethernet/W5500/w5500.o: \
+ ../Drivers/Wiznet_Driver/Ethernet/W5500/w5500.c \
+ ../Drivers/Wiznet_Driver/Ethernet/W5500/../../../Wiznet_Driver/Ethernet/W5500/w5500.h \
+ ../Drivers/Wiznet_Driver/Ethernet/W5500/../../../Wiznet_Driver/Ethernet/W5500/../wizchip_conf.h \
+ ../Drivers/Wiznet_Driver/Ethernet/W5500/../../../Wiznet_Driver/Ethernet/W5500/../../../Wiznet_Driver/Ethernet/W5500/w5500.h
+
+../Drivers/Wiznet_Driver/Ethernet/W5500/../../../Wiznet_Driver/Ethernet/W5500/w5500.h:
+
+../Drivers/Wiznet_Driver/Ethernet/W5500/../../../Wiznet_Driver/Ethernet/W5500/../wizchip_conf.h:
+
+../Drivers/Wiznet_Driver/Ethernet/W5500/../../../Wiznet_Driver/Ethernet/W5500/../../../Wiznet_Driver/Ethernet/W5500/w5500.h:
